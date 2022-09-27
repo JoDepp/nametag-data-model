73295654aeb71eed216b44888be872cbb04a1387
jody_d <jodydepp@gmail.com>
2022-09-27 12:03:46 -0400

    Data Model for name tags challenge

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const names = [
+  {
+    id: "1",
+    person: "Miriam"
+  },
+  {
+    id: "2",
+    person: "Kristy"
+  },
+  {
+    id: "3",
+    person: "Monica"
+  },
+  {
+    id: "4",
+    person: "Amber"
+  },
+  {
+    id: "5",
+    person: "Stephanie"
+  }
+];
